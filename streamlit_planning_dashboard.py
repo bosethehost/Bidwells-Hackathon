5f6cc5384d8bdf720b75d8648ab4b8cb7752665b
Souhridyo Bose <zceisb4@ucl.ac.uk>
2025-11-22 15:09:01 +0000

    Update print statement from 'Hello' to 'Goodbye'

--- streamlit_planning_dashboard.py
+++ streamlit_planning_dashboard.py
@@ -12,7 +12,7 @@ import plotly.graph_objects as go
 # ----------------------------
 # Config - path to uploaded excel
 # ----------------------------
-EXCEL_PATH = "/mnt/data/data.xlsx"  # uploaded file
+EXCEL_PATH = "C:/Users/indra/Desktop/Coding Projects/data.xlsx"  # uploaded file
 
 # ----------------------------
 # Policy weight function (unchanged)
@@ -473,3 +473,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
